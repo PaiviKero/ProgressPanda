e60c7bba18c90cb9e03f8eff43e62fca3c5a7b1e
Päivi Kero <paivi.kero@gmail.com>
2024-08-29 16:02:25 +0300

    set host as 0.0.0.0 and remove dist from repository (woops)

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -24,7 +24,8 @@ app.use('/api/goals', goalRoutes);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || "localhost";
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on ${HOST}:${PORT}`);
 });
