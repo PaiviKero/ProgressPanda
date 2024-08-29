837c00a8fa618de8d5054cdd80c20b54c01e4ded
Päivi Kero <paivi.kero@gmail.com>
2024-08-29 15:11:22 +0300

    origin fix

--- backend/src/config/cors.config.ts
+++ backend/src/config/cors.config.ts
@@ -3,13 +3,7 @@ import  { CorsOptions } from 'cors';
 const allowedOrigins = ['http://localhost:4200'];
 
 export const corsOptions: CorsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: allowedOrigins,
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
   credentials: true, // Allow credentials (cookies, authorization headers)
