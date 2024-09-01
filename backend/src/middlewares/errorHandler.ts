f17dadd68f6e0a5a770eba31f973df65c59a6c03
Päivi Kero <paivi.kero@gmail.com>
2024-09-01 18:56:25 +0200

    fix test error and create linting error to test deploy

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -1,7 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 
-/* eslint  @typescript-eslint/no-unused-vars: [1, { vars: 'all', 'argsIgnorePattern': 'next' } ] */
-const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   console.error(err);
   res.status(500).json({
     message: err.message || 'Internal Server Error',
